75ebeaef8637d8b7a425bcd249e255f2699ee4b4
Shervin <69309473+shervin-h@users.noreply.github.com>
2023-05-07 06:27:29 -0700

    Update constants.dart

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -1,10 +1,10 @@
 class Constants {
   //////// openweathermap section /////////
   static const String baseUrl = 'https://api.openweathermap.org';
-  static const String apiKey1 = '78ae1c8b3241d915817b8b97b4fa5bfa';
-  static const String apiKey2 = '212e82226f0935c4ee6638583c54eb8a';
-  static const String apiKey3 = '9083c5dc1983ea3305d5af027324656f';
+  static const String apiKey1 = 'YOUR FIRST WEATHER MAP API KEY';
+  static const String apiKey2 = 'YOUR SECOND WEATHER MAP API KEY';
+  static const String apiKey3 = 'YOUR THIRD WEATHER MAP API KEY';
 
   //////// neshan section /////////////////
-  static const String neshanApiKey = 'service.9644b09c88ad4598b1763c690cc0e411';
+  static const String neshanApiKey = 'YOUR NESHAN API KEY';
 }
